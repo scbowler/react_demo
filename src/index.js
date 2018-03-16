db44422f8805af1650f10444591e20cbcfabf657
Scott Bowler <scott.bowler@learningfuze.com>
2018-03-16 16:34:19 -0700

    Intro demo complete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+    firstName: 'Peter',
+    lastName: 'Parker'
+}
+
+function greetUser(user){
+    return user.firstName + ' ' + user.lastName;
+}
+
+const greeting = <h1>Hello {greetUser(user)}</h1>;
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
